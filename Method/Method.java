617251d622dc7fa06c2cfa2d2ba5fca9a05046fb
ASHISH GAUTAM <ashish.gautam.254681@gmail.com>
2026-06-08 11:41:00 +0530

    Add files via upload

---
+++ Method/Method.java
@@ -0,0 +1,32 @@
+
+public class Method {
+	
+	static
+	{
+		System.out.print("this is static block");
+	}
+   static{
+		System.out.println("this is instance block");
+	}
+   
+   
+Method(){
+	System.out.println("this is constructor block");
+}
+
+	 void m1() {
+		 System.out.println("this is instance method");
+	 }
+	static void m2() {
+		System.out.println("this is static method");
+	}
+	
+	public static void main(String[] args) {
+	Method m=new Method();
+	m.m1();
+	Method.m2();
+	
+
+	}
+
+}
